a7b64fb39eed2af11d562f8d3d086067fb190178
Your Name <samara1381@mail.ru>
2022-02-07 10:17:50 +0300

    task1-1

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,6 +1,22 @@
 function solveEquation(a, b, c) {
   let arr;
-  // код для задачи №1 писать здесь
+  let arr = [];
+  let d = b**2-4*a*c;
+
+  console.log('Discriminant: ' + d);
+
+  if (d < 0)
+  return false;
+
+    if(d == 0) {
+        arr["Quadratic roots"] = (-b + Math.sqrt(d)) / (2 * a);
+    }
+
+    else if(d > 0) {
+        let roots = [];
+        arr.push((-b + Math.sqrt(d)) / (2 * a));
+        arr.push((-b - Math.sqrt(d)) / (2 * a));
+    }
   return arr; // array
 }
 
