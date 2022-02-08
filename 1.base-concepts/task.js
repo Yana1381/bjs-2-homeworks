167d889877bd3298dc453e1327d3942006982763
Your Name <samara1381@mail.ru>
2022-02-08 09:54:56 +0300

    fixed task 1-1

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,29 +1,23 @@
 function solveEquation(a, b, c) {
-  let arr;
   let arr = [];
   let d = b**2-4*a*c;
 
   console.log('Discriminant: ' + d);
 
-  if (d < 0)
-  return false;
-
     if(d == 0) {
-        arr["Quadratic roots"] = (-b + Math.sqrt(d)) / (2 * a);
+        arr.push(-b/(2*a));
     }
-
     else if(d > 0) {
-        let roots = [];
         arr.push((-b + Math.sqrt(d)) / (2 * a));
         arr.push((-b - Math.sqrt(d)) / (2 * a));
     }
-  return arr; // array
+  return arr; 
 }
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
   let totalAmount;
 
-  // код для задачи №2 писать здесь
+  // код для задачи №2 писать здесь   
 
   return totalAmount;
 }
