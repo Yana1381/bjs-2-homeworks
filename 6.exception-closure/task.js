e22aa32a547813bb9ec7c19a01b48595cba693c6
Your Name <samara1381@mail.ru>
2022-03-04 10:10:31 +0300

    Exeption V2

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -18,3 +18,36 @@ function validateCount(value) {
 
 //Задание 2
 
+class Triangle {    
+	constructor(a, b, c) {
+    this.a = a;
+    this.b = b;
+    this.c = c;
+    if ((a + b) < c || (a + c) < b || (c + b) < a) {
+      throw new Error('Треугольник с такими сторонами не существует');
+    }
+  }
+
+  getPerimeter() {
+    let perimeter = this.a + this.b + this.c;
+    return perimeter;
+  }
+    
+  getArea() {
+    let p = (this.a + this.b + this.c) / 2;
+    let S = Math.sqrt(p * (p - this.a) * (p - this.b) * (p - this.c));
+	S = +S.toFixed(4);
+    return S;
+  }
+}
+
+function getTriangle(a, b, c) {
+  try {
+    return new Triangle;
+  } 
+  catch (e) {
+    return new Triangle,
+    getPerimeter(), getArea();
+    console.log('Ошибка! Треугольник не существует');
+  }
+}
